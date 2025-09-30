23b91328d7100da78160a4b3cf20590518559f83
Saima Sanjeeda Khan <119809514+saima-khan1@users.noreply.github.com>
2025-09-30 14:39:33 +0200

    fixed package issue

--- my-portfolio/frontend/src/App.tsx
+++ my-portfolio/frontend/src/App.tsx
@@ -1,6 +1,13 @@
 import "tailwindcss";
+import Home from "./components/Home";
+
 const App = () => {
-  return <h1 className="text-5xl text-blue-500">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-5xl text-blue-500 bg-yellow-200">Hello world!</h1>
+      <Home />
+    </div>
+  );
 };
 
 export default App;
---
+++ my-portfolio/frontend/src/components/Home.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <h1 className="text-5xl text-blue-500 bg-yellow-200">Hello world!</h1>;
+};
+
+export default Home;
